75aa8addef390f0e42fb8ed40b1b2fafff3f4bb6
estranium <estranium@users.noreply.github.com>
2022-07-15 18:55:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,41 +9,57 @@ const putInCache = async (request, response) => {
 };
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-  // First try to get the resource from the cache
-  const responseFromCache = await caches.match(request);
-  if (responseFromCache) {
-    return responseFromCache;
-  }
-
-  // Next try to use the preloaded response, if it's there
-  const preloadResponse = await preloadResponsePromise;
-  if (preloadResponse) {
-    console.info('using preload response', preloadResponse);
-    putInCache(request, preloadResponse.clone());
-    return preloadResponse;
-  }
-
-  // Next try to get the resource from the network
   try {
-    const responseFromNetwork = await fetch(request);
-    // response may be used only once
-    // we need to save clone to put one copy in cache
-    // and serve second one
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
+      return caches.match(request).then((cachedResponse) => {
+        const fetchedResponse = fetch(request).then((networkResponse) => {
+          //cache.put(event.request, networkResponse.clone());
+		  putInCache(request, networkResponse.clone());
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
   } catch (error) {
-//    const fallbackResponse = await caches.match(fallbackUrl);
-//    if (fallbackResponse) {
-//      return fallbackResponse;
-//    }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
     return new Response('Network error happened', {
       status: 408,
       headers: { 'Content-Type': 'text/plain' },
     });
   }
+//   // First try to get the resource from the cache
+//   const responseFromCache = await caches.match(request);
+//   if (responseFromCache) {
+//     return responseFromCache;
+//   }
+
+//   // Next try to use the preloaded response, if it's there
+//   const preloadResponse = await preloadResponsePromise;
+//   if (preloadResponse) {
+//     console.info('using preload response', preloadResponse);
+//     putInCache(request, preloadResponse.clone());
+//     return preloadResponse;
+//   }
+
+//   // Next try to get the resource from the network
+//   try {
+//     const responseFromNetwork = await fetch(request);
+//     // response may be used only once
+//     // we need to save clone to put one copy in cache
+//     // and serve second one
+//     putInCache(request, responseFromNetwork.clone());
+//     return responseFromNetwork;
+//   } catch (error) {
+// //    const fallbackResponse = await caches.match(fallbackUrl);
+// //    if (fallbackResponse) {
+// //      return fallbackResponse;
+// //    }
+//     // when even the fallback response is not available,
+//     // there is nothing we can do, but we must always
+//     // return a Response object
+//     return new Response('Network error happened', {
+//       status: 408,
+//       headers: { 'Content-Type': 'text/plain' },
+//     });
+//   }
 };
 
 const enableNavigationPreload = async () => {
